0b1f73c4362bf2e1f2214310675bd7638727b68c
Danilo <daniloworkdev@gmail.com>
2022-04-24 13:34:46 +0100

    ex_01 intro

---
+++ 1_INTRO/ex_01/index.js
@@ -0,0 +1,6 @@
+var num = 10;
+var num2 = 20;
+function som(n, n2) {
+    console.log(n + n2);
+}
+som(num, num2);
---
+++ 1_INTRO/ex_01/index.ts
@@ -0,0 +1,8 @@
+const num: number = 10
+const num2: number = 20
+
+function som(n: number, n2: number){
+    console.log(n + n2)
+}
+
+som(num, num2)
