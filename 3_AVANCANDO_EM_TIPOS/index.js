3b3cebde06fe1263a81462a0c13c0a3d7c786e65
Danilo <daniloworkdev@gmail.com>
2022-04-24 14:11:51 +0100

    Tipos_Array

---
+++ 3_AVANCANDO_EM_TIPOS/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// 1 - arrays
+let numbers = [1, 2, 3];
+numbers.push(5);
+console.log(numbers);
+// numbers = "teste" vai dar erro!
+const nomes = ["Danilo", "João"];
+// nomes.push(4) vai dar erro!
+// 2 - outra sintaxe array
+const nums = [100, 200, 3];
+nums.push(300);
+console.log(nums);
+// nums.push("teste") vai dar erro!
---
+++ 3_AVANCANDO_EM_TIPOS/index.ts
@@ -0,0 +1,22 @@
+// 1 - arrays
+let numbers: number[] = [1, 2, 3]
+
+numbers.push(5)
+
+console.log(numbers)
+
+// numbers = "teste" vai dar erro!
+
+const nomes: string[] = ["Danilo", "João"]
+
+// nomes.push(4) vai dar erro!
+
+// 2 - outra sintaxe array
+const nums: Array<number> = [100, 200, 3]
+
+nums.push(300)
+
+console.log(nums)
+
+// nums.push("teste") vai dar erro!
+
