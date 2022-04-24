eee822998f1d18e01b1f65d01ca6c793aee31c18
Danilo <daniloworkdev@gmail.com>
2022-04-24 14:14:29 +0100

    Tipos_Any

--- 3_AVANCANDO_EM_TIPOS/index.js
+++ 3_AVANCANDO_EM_TIPOS/index.js
@@ -11,3 +11,8 @@ const nums = [100, 200, 3];
 nums.push(300);
 console.log(nums);
 // nums.push("teste") vai dar erro!
+// 3 - any
+const arr1 = [1, "teste", true, { nome: "matheus" }, []];
+console.log(arr1);
+arr1.push([1, 2, 3]);
+console.log(arr1);
--- 3_AVANCANDO_EM_TIPOS/index.ts
+++ 3_AVANCANDO_EM_TIPOS/index.ts
@@ -20,3 +20,11 @@ console.log(nums)
 
 // nums.push("teste") vai dar erro!
 
+// 3 - any
+const arr1: any = [1, "teste", true, {nome:"matheus"}, []]
+
+console.log(arr1)
+
+arr1.push([1,2,3])
+
+console.log(arr1)
