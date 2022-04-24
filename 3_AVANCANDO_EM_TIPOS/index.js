273498622918cf721619b9a77bfc78b8eb5c68c7
Danilo <daniloworkdev@gmail.com>
2022-04-24 14:21:45 +0100

    Tipo_Func

--- 3_AVANCANDO_EM_TIPOS/index.js
+++ 3_AVANCANDO_EM_TIPOS/index.js
@@ -16,3 +16,22 @@ const arr1 = [1, "teste", true, { nome: "matheus" }, []];
 console.log(arr1);
 arr1.push([1, 2, 3]);
 console.log(arr1);
+// 4- parametros tipados
+function soma(a, b) {
+    console.log(a + b);
+}
+soma(5, 5);
+//soma("5a","d")
+// 5- retorno funcção
+function greeting(name) {
+    // return 5 erro de tipo
+    return `Olá ${name}`;
+}
+console.log(greeting("Danilo"));
+// console.log(greeting(123)) erro de tipo
+// 6 - função anonima
+setTimeout(() => {
+    const sallary = 1000;
+    //console.log(parseFloat(sallary)) erro de tipo na func anonima
+    //console.log(sallary)
+});
--- 3_AVANCANDO_EM_TIPOS/index.ts
+++ 3_AVANCANDO_EM_TIPOS/index.ts
@@ -28,3 +28,30 @@ console.log(arr1)
 arr1.push([1,2,3])
 
 console.log(arr1)
+
+// 4- parametros tipados
+
+function soma(a:number , b:number) {
+    console.log(a + b)
+}
+soma(5,5)
+
+//soma("5a","d")
+
+// 5- retorno funcção
+
+function greeting(name:string): string {
+    // return 5 erro de tipo
+    return `Olá ${name}`
+}
+
+console.log(greeting("Danilo"))
+// console.log(greeting(123)) erro de tipo
+
+// 6 - função anonima
+setTimeout(() => {
+    const sallary: number = 1000
+    //console.log(parseFloat(sallary)) erro de tipo na func anonima
+
+    //console.log(sallary)
+})
