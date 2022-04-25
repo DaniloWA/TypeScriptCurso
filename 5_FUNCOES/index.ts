922c91b2e51e9ff32981545abb3db51e992aa066
Danilo <daniloworkdev@gmail.com>
2022-04-25 10:48:26 +0100

    Funcoes_CallbackArgumentos

--- 5_FUNCOES/index.ts
+++ 5_FUNCOES/index.ts
@@ -4,3 +4,20 @@ function withoutReturn():void{
 }
 
 withoutReturn()
+
+// 2 - callback como argumento
+function greeting(name: string): string {
+    return `Olá ${name}`
+}
+
+function preGreeting(f: (name: string) => string, userName: string){
+
+    console.log("Preparando a função")
+
+    const greet = f(userName)
+
+    console.log(greet)
+}
+
+preGreeting(greeting, "Danilo")
+preGreeting(greeting, "Joao")
